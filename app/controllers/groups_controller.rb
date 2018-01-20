c303dac1343c3e3d2d89c0c855d01de621419757
keigo <keeeeeeeeigo1129@gmail.com>
2018-01-20 15:47:17 +0900

    modify view files

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,16 +3,16 @@ class GroupsController < ApplicationController
   end
 
   def new
-    @group=Group.new
-    @group.users << current_user
+    @group = Group.new
   end
 
   def create
-    @group=Group.new(group_params)
+    @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: "グループを作成しました"
     else
-      render :new
+      render :new, alert: "グループの作成に失敗しました"
     end
   end
 
