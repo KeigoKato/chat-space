28637f1797712e0dc538f3dea658a3de46fbae38
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-02-04 14:31:49 +0900

    return message data by using json and jbuilder

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,27 @@
 $(function() {
   $("#new_message").on("submit", function(e) {  //イベントはsubmitボタンではなくformにかける。formのidまたはクラスを参照すること。
     e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action");
+
+    for(item of formData){
+      console.log(item);
+    }
+    console.log(url);
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+    })
+    .fail(function(){
+      alert("failed");
+    })
   });
 });
 
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.name @message.user.name
+json.created_time @message.created_at.to_s
+json.content  @message.content
+json.image  @message.image.to_s
+
