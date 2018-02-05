ca1b77110c4a3b013e51c331f8988ae01dbb9d5b
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-02-05 21:54:54 +0900

    implement user append and remove function

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -13,6 +13,16 @@ $(document).on("turbolinks:load", function(){   //あまり気にせずに行き
               </div>`
     $(".user-search-result").append(html);
   }
+
+  function addToGroup(user){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user.id}'>
+                    <p class='chat-group-user__name'>${user.name}</p>
+                    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    $("#chat-group-users").append(html);
+  }
+
   $(".chat-group-form__field").on("keyup", function(){
     var input = $("#user-search-field").val();
     if (input){
@@ -29,8 +39,7 @@ $(document).on("turbolinks:load", function(){   //あまり気にせずに行き
             appendUser(user);
           });
         }
-        else {
-          absenceUser();
+        else { absenceUser();
         }
       })
       .fail(function(){
@@ -40,5 +49,16 @@ $(document).on("turbolinks:load", function(){   //あまり気にせずに行き
     else {
       $(".user-search-result").empty();
     }
+  });
+  $(".user-search-result").on("click", ".user-search-add", function(){
+    //.user-search-resultのclickアクションが実行された時に、そのclickの発生元が.user-search-addであればfunctionを実行する。
+    //バブリングは親要素に対して遡って行く。
+    // console.log("done click event");
+    $(this).parent().remove();
+    var addUser = {"id": $(this).data("user-id"), "name": $(this).data("user-name")};
+    addToGroup(addUser);
+  });
+  $("#chat-group-users").on("click", ".user-search-remove", function(){
+    $(this).parent().remove();
   });
 });
