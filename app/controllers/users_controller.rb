a90569a8b9999d6b035c5c829a127ce455523be6
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-02-06 21:41:05 +0900

    modify incremental search function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where("name LIKE(?)", "%#{params[:keyword]}%")
+    allUsers = User.where("name LIKE(?)", "%#{params[:keyword]}%")
+    @users=allUsers.reject{|user| user.id == current_user.id}
     respond_to do |format|
       format.json
     end
